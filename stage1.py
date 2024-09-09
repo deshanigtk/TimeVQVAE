fda416fcb67a31b89886e9d22938c6c4588b2265
deshanigtk <deshanigeethika@gmail.com>
2024-09-09 19:09:14 +1000

    change back the module import to torch

--- stage1.py
+++ stage1.py
@@ -7,7 +7,7 @@ import os
 from argparse import ArgumentParser
 import copy
 
-import pytorch
+import torch
 import wandb
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor
